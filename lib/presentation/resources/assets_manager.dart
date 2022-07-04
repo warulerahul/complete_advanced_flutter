7b4cc6c3e8625fffe37e06d710ed16c2a1d1ada2
Rahul Warule <rahul.warule@iqdigi.com>
2022-07-04 12:58:28 +0530

    Lecture 25 - Adding Assets for Onboarding Screen

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const String imagePath = "assets/images";
 
 class ImageManager {
   static const String splashLogo = "$imagePath/splash_logo.png";
+  static const String onBoardingLogo1 = "$imagePath/onBoarding_logo1.svg";
+  static const String onBoardingLogo2 = "$imagePath/onBoarding_logo2.svg";
+  static const String onBoardingLogo3 = "$imagePath/onBoarding_logo3.svg";
+  static const String onBoardingLogo4 = "$imagePath/onBoarding_logo4.svg";
 }
