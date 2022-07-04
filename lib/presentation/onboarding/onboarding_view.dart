10fe694c1a0ba5f947b44cf54aef9343473d1d93
Rahul Warule <rahul.warule@iqdigi.com>
2022-07-04 13:26:33 +0530

    Lecture 26 - Implementation Onboarding Screen Part1

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,14 +1,69 @@
-import 'package:complete_advanced_flutter/presentation/resources/color_manager.dart';
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class OnBoardingView extends StatelessWidget {
+import 'package:complete_advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../resources/assets_manager.dart';
+
+class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderDate();
+
+  final PageController _pageController = PageController(initialPage: 0);
+
+  List<SliderObject> _getSliderDate() => [
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageManager.onBoardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle2,
+            AppStrings.onBoardingSubTitle2, ImageManager.onBoardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageManager.onBoardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageManager.onBoardingLogo4),
+      ];
+  
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: const Center(child: Text("Welcome to on boarding")),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index){
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index){
+            //return OnboardingPage;
+          }),
     );
   }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
